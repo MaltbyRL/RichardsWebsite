a653eaa47f7da088bd6b1c34f765b23dca631a02
MaltbyRL <RLMaltby@gmail.com>
2016-02-29 02:22:52 -0800

    used gulp with website build

---
+++ js/bundle.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var app = angular.module('mySite', ['ngRoute', 'ui.router']);
+console.log("app:", app)
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'partials/splash.html',
+      controller: 'homeCtrl'
+    })
+    .state('aboutMe', {
+      url: '/aboutMe',
+      templateUrl: 'partials/about.html',
+      controller: 'aboutCtrl'
+    })
+    .state('blog', {
+      url: '/blog',
+      templateUrl: '../partials/blog.html',
+      controller: 'blogCtrl'
+    })
+    .state('portfolio', {
+      url: '/portfolio',
+      templateUrl: '../partials/portfolio.html',
+      controller: 'portfolioCtrl'
+    })
+
+
+  $urlRouterProvider.otherwise('/');
+});
+
+'use strict';
+
+var app = angular.module('mySite');
+
+app.controller('aboutCtrl', function() {
+  
+  $("#backgroundOverlay").addClass('overlay')
+});
+
+'use strict';
+
+var app = angular.module('mySite');
+
+app.controller('blogCtrl', function() {
+  
+  $("#backgroundOverlay").addClass('overlay')
+});
+
+'use strict';
+
+var app = angular.module('mySite');
+
+app.controller('homeCtrl', function() {
+
+  $(".overlay").removeClass()
+});
+
+'use strict';
+
+var app = angular.module('mySite');
+
+app.controller('portfolioCtrl', function($scope) {
+
+    $("#backgroundOverlay").addClass('overlay')
+
+
+});
