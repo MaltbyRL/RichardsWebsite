88fc0bcc487404586585b126bde9466c4ca802aa
MaltbyRL <RLMaltby@gmail.com>
2016-02-29 09:10:50 -0800

    fixed bugs in module.js

--- js/bundle.js
+++ js/bundle.js
@@ -4,11 +4,6 @@ var app = angular.module('mySite', ['ngRoute', 'ui.router']);
 console.log("app:", app)
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'partials/splash.html',
-      controller: 'homeCtrl'
-    })
     .state('aboutMe', {
       url: '/aboutMe',
       templateUrl: 'partials/about.html',
@@ -16,13 +11,18 @@ app.config(function($stateProvider, $urlRouterProvider) {
     })
     .state('blog', {
       url: '/blog',
-      templateUrl: '../partials/blog.html',
+      templateUrl: 'partials/blog.html',
       controller: 'blogCtrl'
     })
     .state('portfolio', {
       url: '/portfolio',
-      templateUrl: '../partials/portfolio.html',
+      templateUrl: 'partials/portfolio.html',
       controller: 'portfolioCtrl'
+    })
+    .state('home', {
+      url: '/',
+      templateUrl: 'partials/splash.html',
+      controller: 'homeCtrl'
     })
 
 
@@ -34,7 +34,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 var app = angular.module('mySite');
 
 app.controller('aboutCtrl', function() {
-  
+
   $("#backgroundOverlay").addClass('overlay')
 });
 
@@ -43,7 +43,7 @@ app.controller('aboutCtrl', function() {
 var app = angular.module('mySite');
 
 app.controller('blogCtrl', function() {
-  
+
   $("#backgroundOverlay").addClass('overlay')
 });
 
