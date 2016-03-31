593f7ba3954f1dfa975c9272b49700a882473ece
MaltbyRL <RLMaltby@gmail.com>
2016-03-31 11:20:40 -0700

    added pieces to portfolio

--- js/bundle.js
+++ js/bundle.js
@@ -35,6 +35,61 @@ app.run(function($window, $rootScope, $location) {
 });
 });
 
+
+
+
+
+/*
+ * Complete the function below.
+ */
+function decentNumber(arr) {
+console.log(arr)
+var decentArr = [];
+
+    var makeIt = function(n){
+        var newArr = [];
+        var ele = 0;
+        for(var i = 0; i < n; i++){
+            if(i === 0){
+                for(var x = i; x < n; x++){
+                newArr.push(5);
+                }
+            }
+            console.log(newArr)
+            if(n % 3 === 0){
+                return decentArr.push(newArr.join(""));
+            }else{
+                for(b = 0; b < 5; b++){
+                    console.log("__________")
+                    newArr[n - 1 - ele] = 3;
+                    ele++
+                }
+                if((ele)%5 === 0){
+                    console.log("...........")
+                    return decentArr.push(newArr.join(""))
+                    }
+            }
+        }
+    }
+
+
+
+    for(i=0; i<arr.length; i++){
+        if(arr[i] < 3){
+            decentArr.push(-1)
+        }else{
+            console.log(i)
+            console.log("decArr",decentArr)
+            var decNum = makeIt(arr[i]);
+            console.log("decArr2", decentArr)
+        }
+    }
+
+
+    console.log(decentArr)
+
+}
+
 'use strict';
 
 var app = angular.module('mySite');
